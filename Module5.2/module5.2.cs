e08c9979ef5af5a56a33db9b049d8c412a0ccb3e
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-10-14 02:10:31 +0000

    started 5.2

---
+++ Module5.2/module5.2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    const int numTemps = 5;
+    const int minTemp = -30;
+    const int maxTemp = 130;
+    int[] temperatures = new int[numTemps];
+    int temp;
+
+    for (int i = 0; i < numTemps; i++)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write($"Enter temperature {i + 1}; ");
+                temp = int.Parse(Console.ReadLine());
+                if (temp < minTemp )
+            }
+        }
+    }
+  }
+}
