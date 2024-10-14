ff66f6654ce4f8e2af9908b2b635790513375f22
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-10-14 03:33:47 +0000

    finished 5.2 module

--- Module5.2/module5.2.cs
+++ Module5.2/module5.2.cs
@@ -7,18 +7,69 @@ class Program {
     const int maxTemp = 130;
     int[] temperatures = new int[numTemps];
     int temp;
+    string? input;
 
     for (int i = 0; i < numTemps; i++)
     {
         while (true)
         {
-            try
-            {
-                Console.Write($"Enter temperature {i + 1}; ");
-                temp = int.Parse(Console.ReadLine());
-                if (temp < minTemp )
+                Console.Write($"Enter temperature {i + 1}: ");
+                input = Console.ReadLine();
+
+                if (input == null)
+                {
+                    Console.WriteLine($"Error: No input provided. Please enter a valid temperature.");
+                    continue;
+                    }
+                    try
+                    {
+                    temp = int.Parse(input); 
+                    
+                    if (temp < minTemp || temp > maxTemp)
+                    {
+                    throw new Exception($"Temperature {temp} is invalid. Please enter a valid temperature between {minTemp} and {maxTemp}.");
+                    }
+                    temperatures[i] = temp;
+                    break;
+                
+                }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("check expected outcome");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
             }
-        }
+            
+    bool gettingWarmer = true;
+    bool gettingCooler = true;
+    {
+    for (int i = 1; i < numTemps; i++)
+    {
+        if (temperatures[i] < temperatures[i-1])
+            gettingWarmer = false;
+        if (temperatures[i] > temperatures[i-1])
+            gettingCooler = false;
+        
     }
+    if (gettingWarmer)
+    {
+        Console.WriteLine("Getting Warmer");
+    }
+    else if (gettingCooler)
+    {
+        Console.WriteLine("Getting Cooler");
+    }
+    else
+    {
+        Console.WriteLine("It's a mixed bag");
+    }
+    Console.WriteLine("5-day Temperatures: [" + string.Join(", ", temperatures) + "]");
+    double averageTemp = temperatures.Average();
+    Console.WriteLine($"Average Temperature is {averageTemp:F1} degrees");
   }
-}
+  }
+  }
