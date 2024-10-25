eafeba948fe72dbbdb1d32a3ca8a2e7475f71481
XiomarRob <157074573+XiomarRob@users.noreply.github.com>
2024-10-25 20:57:27 +0000

    new proj 0-1

---
+++ Project 0-1/Project 0-1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine("************************************");
+    Console.WriteLine("*  The stars shine in Greenville.  *");
+    Console.WriteLine("************************************\n");
+  }
+
+  static int getInput (string prompt)
+  {
+    int result;
+    Console.Write(prompt);
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 0)
+    {
+      Console.Write("Invalid input. Enter a positive valid number: ")
+    }
+    return result;
+  }
+
+  static void Revenue()
+  {
+    Console.WriteLine("")
+  }
+}
